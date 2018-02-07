6171af1fd8a3d38356d4d185c87b0968c460a0b6
Veronica Kim <vero.a.kim@gmail.com>
2018-02-07 02:37:24 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,7 +4,11 @@ def Shoe
   attr_accessor :brand, :color, :size, :material, :condition
   attr_reader
 
+  def initialize(shoe)
+    @shoe
+  end
+    
   def cobble
-    puts "the shoe has been repaired."
+    puts "the shoe has been repaired"
 
 end
