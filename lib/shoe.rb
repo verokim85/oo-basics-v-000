9bbc09cf7173167badafacbae90c1ad7574f9274
Veronica Kim <vero.a.kim@gmail.com>
2018-02-07 02:37:43 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,5 @@
 # Make your shoe class here!
-def Shoe
+class Shoe
 
   attr_accessor :brand, :color, :size, :material, :condition
   attr_reader
@@ -7,7 +7,7 @@ def Shoe
   def initialize(shoe)
     @shoe
   end
-    
+
   def cobble
     puts "the shoe has been repaired"
 
